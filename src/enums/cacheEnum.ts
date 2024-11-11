a04f2419c672b56c813cab692f3c6755e5298679
huhn <2682480394@qq.com>
2024-11-11 10:44:29 +0800

    style: 格式

--- src/enums/cacheEnum.ts
+++ src/enums/cacheEnum.ts
@@ -1,11 +1,11 @@
-// token key
+// 登录 token
 export const TOKEN_KEY = 'TOKEN'
 
-// 系统配置: locale、theme
+// 系统配置: locale 多语言、theme 主题等配置
 export const APP_CONFIG_KEY = 'APP_CONFIG'
 
-// user info key
+// 用户信息
 export const USER_INFO_KEY = 'USER__INFO'
 
-// role info key
+// 系统角色权限
 export const ROLES_KEY = 'ROLES'
